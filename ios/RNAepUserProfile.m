ecbe709745b81e00fe6071800c77ad59f00383cc
richa-adobe <risharma@adobe.com>
2021-02-26 22:21:52 +0530

    Add files via upload

--- ios/RNAepUserProfile.m
+++ ios/RNAepUserProfile.m
@@ -1,6 +1,16 @@
 
+//#import "AEPUserProfile/AEPUserProfile.h"
+// #if __has_include("AEPUserProfile.h")
+// #import "AEPUserProfile.h"
+// #else
+// #import <AEPUserProfile/AEPUserProfile.h>
+// #endif
+
+@import AEPUserProfile;
+
 #import "RNAepUserProfile.h"
-#import "AEPUserProfile.h"
+//#import "AEPCore.h"
+//#import "AEPUserProfile.h"
 #import "React/RCTLog.h"
 
 @implementation RNAepUserProfile
@@ -9,29 +19,32 @@
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_METHOD(extensionVersion: (RCTPromiseResolveBlock) resolve rejecter:(RCTPromiseRejectBlock)reject) {
-    resolve([AEPUserProfile extensionVersion]);
-}
+// RCT_EXPORT_METHOD(extensionVersion: (RCTPromiseResolveBlock) resolve rejecter:(RCTPromiseRejectBlock)reject) {
+//     resolve([AEPMobileUserProfile extensionVersion]);
+// }
 
-RCT_EXPORT_METHOD(registerExtension) {
-    [AEPUserProfile registerExtension];
+RCT_EXPORT_METHOD(AEPUserProfileVersionNumber) {
+    [AEPMobileUserProfile AEPUserProfileVersionNumber];
 }
+// RCT_EXPORT_METHOD(registerExtension) {
+//     [AEPMobileUserProfile registerExtension];
+// }
 
 RCT_EXPORT_METHOD(printMessage) {
     RCTLogInfo(@"Custom Message1");
 }
 
-RCT_EXPORT_METHOD(removeUserAttribute: (nonnull NSString*) attributeName) {
-    [AEPUserProfile removeUserAttribute:attributeName];
-}
+// RCT_EXPORT_METHOD(removeUserAttribute: (nonnull NSString*) attributeName) {
+//     [AEPUserProfile removeUserAttribute:attributeName];
+// }
 
-RCT_EXPORT_METHOD(updateUserAttribute: (nonnull NSString*) attributeName withValue: (nullable NSString*) attributeValue) {
-    [AEPUserProfile updateUserAttribute:attributeName withValue:attributeValue];
-}
+// RCT_EXPORT_METHOD(updateUserAttribute: (nonnull NSString*) attributeName withValue: (nullable NSString*) attributeValue) {
+//     [AEPUserProfile updateUserAttribute:attributeName withValue:attributeValue];
+// }
 
-RCT_EXPORT_METHOD(updateUserAttributes: (nonnull NSDictionary*) attributeMap) {
-    [AEPUserProfile updateUserAttributes:attributeMap];
-}
+// RCT_EXPORT_METHOD(updateUserAttributes: (nonnull NSDictionary*) attributeMap) {
+//     [AEPUserProfile updateUserAttributes:attributeMap];
+// }
 
 RCT_EXPORT_MODULE(RNAepUserProfile);
 
--- ios/RNAepUserProfile.podspec
+++ ios/RNAepUserProfile.podspec
@@ -10,14 +10,14 @@ Pod::Spec.new do |s|
   s.license      = "MIT"
   # s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
   s.author             = { "author" => "author@domain.cn" }
-  s.platform     = :ios, "7.0"
+  s.platform     = :ios, "9.0"
   s.source       = { :git => "https://github.com/richa-adobe/react-native-profile.git", :tag => "#{s.version}" }
   s.source_files  = "*.{h,m}"
   s.requires_arc = true
 
-
+  s.pod_target_xcconfig = { 'BUILD_LIBRARY_FOR_DISTRIBUTION' => 'YES' }
   s.dependency "React"
-  s.dependency "AEPUserProfile","3.0.0"
+  s.dependency "AEPUserProfile","~> 3.0.0"
   s.frameworks = 'UIKit', 'SystemConfiguration', 'WebKit', 'UserNotifications'
   s.library = 'sqlite3.0', 'c++', 'z'
   #s.dependency "others"
